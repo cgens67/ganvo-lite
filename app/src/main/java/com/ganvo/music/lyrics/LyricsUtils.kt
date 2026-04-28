3d1c81caf306839f29c89028fdce7dffe1ca774a
cgens67 <cgenshin587@gmail.com>
2026-04-28 15:45:30 +0800

    Update LyricsUtils.kt

--- app/src/main/java/com/ganvo/music/lyrics/LyricsUtils.kt
+++ app/src/main/java/com/ganvo/music/lyrics/LyricsUtils.kt
@@ -15,34 +15,24 @@ object LyricsUtils {
             }.sorted()
 
     private fun parseLine(line: String): List<LyricsEntry>? {
-        if (line.isEmpty()) {
-            return null
-        }
+        if (line.isEmpty()) return null
         val matchResult = LINE_REGEX.matchEntire(line.trim()) ?: return null
         val times = matchResult.groupValues[1]
         val text = matchResult.groupValues[3]
         val timeMatchResults = TIME_REGEX.findAll(times).toList()
-        
         if (timeMatchResults.isEmpty()) return null
-
-        return timeMatchResults
-            .map { timeMatchResult ->
-                val min = timeMatchResult.groupValues[1].toLong()
-                val sec = timeMatchResult.groupValues[2].toLong()
-                val milString = timeMatchResult.groupValues[3]
-                var mil = milString.toLong()
-                if (milString.length == 2) {
-                    mil *= 10
-                }
-                val time = min * DateUtils.MINUTE_IN_MILLIS + sec * DateUtils.SECOND_IN_MILLIS + mil
-                LyricsEntry(time, text)
-            }.toList()
+        return timeMatchResults.map { timeMatchResult ->
+            val min = timeMatchResult.groupValues[1].toLong()
+            val sec = timeMatchResult.groupValues[2].toLong()
+            val milString = timeMatchResult.groupValues[3]
+            var mil = milString.toLong()
+            if (milString.length == 2) mil *= 10
+            val time = min * DateUtils.MINUTE_IN_MILLIS + sec * DateUtils.SECOND_IN_MILLIS + mil
+            LyricsEntry(time, text)
+        }.toList()
     }
 
-    fun findCurrentLineIndex(
-        lines: List<LyricsEntry>,
-        position: Long,
-    ): Int {
+    fun findCurrentLineIndex(lines: List<LyricsEntry>, position: Long): Int {
         for (index in lines.indices) {
             if (lines[index].time >= position + ANIMATE_SCROLL_DURATION) {
                 return index - 1
