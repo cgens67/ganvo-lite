8aa85e49e85cb218217f77ac520365655f318a9c
cgens67 <cgenshin587@gmail.com>
2026-05-09 21:30:54 +0800

    Create BetterLyricsProvider.kt

---
+++ app/src/main/java/com/ganvo/music/lyrics/BetterLyricsProvider.kt
@@ -0,0 +1,19 @@
+package com.ganvo.music.lyrics
+
+import android.content.Context
+import com.metrolist.music.betterlyrics.BetterLyrics
+
+object BetterLyricsProvider : LyricsProvider {
+    override val name = "BetterLyrics (Apple Music)"
+
+    override fun isEnabled(context: Context) = true
+
+    override suspend fun getLyrics(
+        id: String,
+        title: String,
+        artist: String,
+        duration: Int,
+    ): Result<String> {
+        return BetterLyrics.getLyrics(title, artist, duration, null)
+    }
+}
