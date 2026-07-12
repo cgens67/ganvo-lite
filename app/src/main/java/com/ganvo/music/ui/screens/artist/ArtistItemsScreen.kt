42178b2dfd15ac0ea76f5ae3a12680d8ab2ded99
cgens67 <cgenshin587@gmail.com>
2026-07-12 13:59:52 +0800

    Update ArtistItemsScreen.kt

--- app/src/main/java/com/ganvo/music/ui/screens/artist/ArtistItemsScreen.kt
+++ app/src/main/java/com/ganvo/music/ui/screens/artist/ArtistItemsScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
